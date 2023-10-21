09c8e07f3778505232c86376ad81662dae1d06e0
Ian <hi@ian.pe>
2023-10-21 19:08:37 +0100

    Add country codes and youtube api key

---
+++ config/country_codes.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'nl'    =>  'Netherlands',
+    'uk'    =>  'United Kingdom',
+    'de'    =>  'Germany',
+    'fr'    =>  'France',
+    'es'    =>  'Spain',
+    'it'    =>  'Italy',
+    'gr'    =>  'Greece'
+];
